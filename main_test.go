94867e5ea74391f5761165b8f9fd27632b0ebbb5
Martin Trautwein <mw.peoplez@gmail.com>
2024-09-28 13:40:14 +0200

    minor main test changes

--- main_test.go
+++ main_test.go
@@ -10,14 +10,12 @@ import (
 func TestListenString(t *testing.T) {
 	cfg := NewConfiguration()
 	defer func() {
-		t.Setenv("LISTEN_PORT", "8080")
-
+		t.Setenv("FLUENTBIT_PORT", "8080")
 	}()
-	_ = os.Setenv("LISTEN_PORT", "")
-	s := ":" + cfg.ListenPort
+	_ = os.Setenv("FLUENTBIT_PORT", "")
+	s := ":" + cfg.FluentBitPort
 	assert.Equal(t, s, ":8080")
-	t.Setenv("LISTEN_PORT", "1028")
-	s = ":" + os.Getenv("LISTEN_PORT")
-	t.Logf("port (s) is: %s", ":"+cfg.ListenPort)
+	t.Setenv("FLUENTBIT_PORT", "1028")
+	s = ":" + os.Getenv("FLUENTBIT_PORT")
 	assert.Equal(t, s, ":1028")
 }

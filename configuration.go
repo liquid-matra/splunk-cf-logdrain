20ff3f1fa88158d82c30a9fdde7739b4cfccbe65
Martin Trautwein <mw.peoplez@gmail.com>
2024-09-28 22:32:19 +0200

    config changes

--- configuration.go
+++ configuration.go
@@ -1,7 +1,10 @@
 package main
 
+import "os"
+
 type Configuration struct {
 	ListenPort     string
+	FluentBitPort  string
 	EnvPrefix      string
 	TransportUrl   string
 	SyslogEndpoint string
@@ -9,11 +12,41 @@ type Configuration struct {
 }
 
 func NewConfiguration() Configuration {
-	return Configuration{
-		ListenPort:     "8080",
+	cfg := Configuration{
+		ListenPort:     "2020",
+		FluentBitPort:  "8080",
 		EnvPrefix:      "splunk-cf-logdrain",
 		TransportUrl:   "",
 		SyslogEndpoint: "localhost:5140",
 		Token:          "",
 	}
+	cfg.ReadFromEnv()
+	return cfg
+}
+
+func (cfg *Configuration) ReadFromEnv() {
+	variableName := "LISTEN_PORT"
+	content, isSet := os.LookupEnv(variableName)
+	if isSet {
+		cfg.ListenPort = content
+	}
+
+	variableName = "TOKEN"
+	content, isSet = os.LookupEnv(variableName)
+	if isSet {
+		cfg.Token = content
+	}
+
+	variableName = "SYSLOG_ENDPOINT"
+	content, isSet = os.LookupEnv(variableName)
+	if isSet {
+		cfg.SyslogEndpoint = content
+	}
+
+	variableName = "FLUENTBIT_PORT"
+	content, isSet = os.LookupEnv(variableName)
+	if isSet {
+		cfg.SyslogEndpoint = content
+	}
+
 }
